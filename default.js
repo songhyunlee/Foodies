30714d19fb68fdb1610f38ce8252d5377ed8a4a2
Songhyun Lee <empressivlee@gmail.com>
2016-05-30 13:43:49 -0700

    Added search items #1

---
+++ default.js
@@ -0,0 +1,72 @@
+var reviews = [
+  {
+    category: ['food', 'american'],
+    name: 'Cheeseboard',
+    reviewer: 'Sarah Anderson',
+    review: 'The best vegetarian pizza I have ever had.'
+    score: 5
+  },
+  {
+    category: ['food', 'asian'],
+    name: 'Great China',
+    reviewer: 'John Doe',
+    review: 'The food in general was all good, but the service was not that great.'
+    score: 4
+  },
+  {
+    category: ['food', 'seafood'],
+    name: 'Oh Oyster',
+    reviewer: 'Jen Kim',
+    review: 'I ordered the daily special, but the oysters were not fresh at all.'
+    score: 2
+  },
+  {
+    category: ['food', 'italian'],
+    name: 'Siciliana',
+    reviewer: 'Ken Peterson',
+    review: 'I do not understand why people line up for this place. It was okay.'
+    score: 3
+  },
+  {
+    category: ['food', 'asian'],
+    name: 'Baekjung',
+    reviewer: 'Remmy Smith',
+    review: 'Korean BBQ. Sooo good. I definitely want to go back again.'
+    score: 5
+  },
+  {
+    category: ['food', 'asian'],
+    name: 'Sushi Sushi',
+    reviewer: 'Jim Miller',
+    review: 'Very low quality. I would rather get sushi from Wholefoods.'
+    score: 1
+  },
+  {
+    category: ['food', 'mexican'],
+    name: 'Tacos o Burritos',
+    reviewer: 'Tucker Venditozzi',
+    review: 'My favorite burritos in town. They should put more meat in them, though.'
+    score: 4
+  },
+  {
+    category: ['food', 'foodtruck', 'american'],
+    name: 'Grab N Go',
+    reviewer: 'Fred Huang',
+    review: 'You do not want to miss out when their truck is in your area. It is just amazing. Order the cuban or the rubenesque.'
+    score: 5
+  },
+  {
+    category: ['food', 'dessert'],
+    name: 'Matcha Heaven',
+    reviewer: 'Pooja Shah',
+    review: 'All their dessert is made with green tea. If you like matcha, this is your place for dessert.'
+    score: 4
+  },
+  {
+    category: ['food', 'dessert'],
+    name: 'Coffee-to-go',
+    reviewer: 'Bri Angotti',
+    review: 'Never coming back to this coffeeshop. Dirty and rude service for very cheap quality coffee.'
+    score: 1
+  },
+]
