eda07b33c91eb3182ef2bb6cf339a1449c3f52ac
Songhyun Lee <empressivlee@gmail.com>
2016-06-03 16:06:44 -0700

    added panels to display results when user searches for a restaurant #1

--- default.js
+++ default.js
@@ -1,79 +1,129 @@
 var reviews = [
-  {   
+  {
     food: 'american',
-    name: 'Cheezboard',
-    reviewer: 'Sarah Anderson',
-    review: 'The best vegetarian pizza I have ever had.',
-    rating: '5'
+    biz: 'Cheezboard',
+    description: 'Vegetarian pizzeria serving different pizza everyday...',
+    reviewer: ['Sarah Anderson', 'Two', 'Three'],
+    review: ['The best vegetarian pizza I have ever had.', 'Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 5,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'cheezboard',
+    image: "cheezboard.jpg",
   },
   {
     food: 'chinese',
-    name: 'Great China',
-    reviewer: 'John Doe',
-    review: 'Delicious food, excellent service',
-    rating: 5
+    biz: 'Great China',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['Delicious food, excellent service','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 5,
+    address: '3333 Bristol St, Costa Mesa CA 92603',
+    phone: '949-567-8901',
+    id: 'greatchina',
+    image: 'greatchina.jpeg',
   },
   {
     food: 'seafood',
-    name: 'Oh Oyster',
-    reviewer: 'Jen Kim',
-    review: 'I ordered the daily special, but the oysters were not fresh at all.',
-    rating: 2
+    biz: 'Oh Oyster',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['I ordered the daily special, but the oysters were not fresh at all.', 'Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 2,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'seafood',
+    image: 'oysters.jpeg',
   },
   {
     food: 'italian',
-    name: 'Siciliana',
-    reviewer: 'Ken Peterson',
-    review: 'I do not understand why people line up for this place. It was okay.',
-    rating: 3
+    biz: 'Siciliana',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['I do not understand why people line up for this place. It was okay.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 3,
+    address: '3333 Bristol St, Costa Mesa CA 92603',
+    phone: '949-567-8901',
+    id: 'italian',
+    image: 'siciliana.jpg',
   },
   {
     food: 'korean',
-    name: 'Baekjung',
-    reviewer: 'Remmy Smith',
-    review: 'Korean BBQ. Sooo good. I definitely want to go back again.',
-    rating: 4
+    biz: 'Baekjung',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['Korean BBQ. Sooo good. I definitely want to go back again.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 4,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'korean',
+    image: 'baekjung.jpg',
   },
   {
     food: 'japanese',
-    name: 'Sushi Sushi',
-    reviewer: 'Jim Miller',
-    review: 'Very low quality. I would rather get sushi from Wholefoods.',
-    rating: 1
+    biz: 'Sushi Sushi',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['Very low quality. I would rather get sushi from Wholefoods.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 1,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'italian',
+    image: 'sushi.jpg',
   },
   {
     food: 'mexican',
-    name: 'Tacos o Burritos',
-    reviewer: 'Tucker Venditozzi',
-    review: 'My favorite burritos in town. They should put more meat in them, though.',
-    rating: 5
+    biz: 'Tacos o Burritos',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['My favorite burritos in town. They should put more meat in them, though.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 5,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'tacos',
+    image: 'burritos.jpg',
   },
   {
     food: 'foodtruck',
-    name: 'Grab N Go',
-    reviewer: 'Fred Huang',
-    review: 'You do not want to miss out when their truck is in your area. It is just amazing. Order the cuban or the rubenesque.',
-    rating: 5
+    biz: 'Grab N Go',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['You do not want to miss out when their truck is in your area. It is just amazing. Order the cuban or the rubenesque.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 4,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'truck',
+    image: 'foodtruck.jpg',
   },
   {
     food: 'dessert',
-    name: 'Matcha Heaven',
-    reviewer: 'Pooja Shah',
-    review: 'All their dessert is made with green tea. If you like matcha, this is your place for dessert.',
-    rating: 4
+    biz: 'Matcha Heaven',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['All their dessert is made with green tea. If you like matcha, this is your place for dessert.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 4,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'heaven',
+    image: 'matcha.jpeg',
   },
   {
     food: 'coffee',
-    name: 'Coffee-to-go',
-    reviewer: 'Bri Angotti',
-    review: 'Never coming back to this coffeeshop. Dirty and rude service for very cheap quality coffee.',
-    rating: 1
+    biz: 'Coffee-to-go',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
+    review: ['Never coming back to this coffeeshop. Dirty and rude service for very cheap quality coffee.','Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
+    rating: 1,
+    address: '1 Culver Dr, Irvine, CA 92612',
+    phone: '949-567-8901',
+    id: 'cafe',
+    image: 'coffee.jpg',
   },
 ]
 
 function fullMatch(term) {
   for (var i = 0; i < reviews.length; i++) {
-    if (term === reviews[i].name) {
+    if (term === reviews[i].biz) {
       return reviews[i];
     }
     if (term === reviews[i].food) {
@@ -88,7 +138,7 @@ function partialMatch(term) {
   var suggestions = [];
 
   for (var i =0; i < reviews.length; i++) {
-  if (reviews[i].name.indexOf(term) !== -1) {
+  if (reviews[i].biz.indexOf(term) !== -1) {
     suggestions.push(reviews[i]);
   }
   if (reviews[i].food.indexOf(term) !== -1) {
@@ -105,21 +155,37 @@ function partialMatch(term) {
 
 var myElement = document.getElementById('search')
 myElement.addEventListener('click', function search(Event) {
-  var removeEl = document.getElementsByClassName('container recommended')[0];
-  var containerEl = removeEl.parentNode;
-  containerEl.removeChild(removeEl);
+
   var term = document.getElementById('term');
   var matched = fullMatch(term.value);
   var part = partialMatch(term.value);
-  var item = document.createElement('p');
+  var item = document.createElement('div');
+  var thePanel = document.createElement('div');
+  var panelHeading = document.createElement('div');
+  var panelBody = document.createElement('div');
+  thePanel.setAttribute('class', 'panel panel-success');
+  panelHeading.setAttribute('class', 'panel-heading');
+  panelBody.setAttribute('class', 'panel-body');
+  item.setAttribute('class', 'results');
+  thePanel.appendChild(panelHeading);
+  thePanel.appendChild(panelBody);
+  item.appendChild(thePanel);
   if ( matched ) {
-    item.textContent = "You've searched for: " + "'"+term.value +"'. " + matched.name + " has a rating of " + matched.rating + ".";
+    panelBody.textContent = "You've searched for: " + "'"+term.value +"'. " + matched.biz + " has a rating of " + matched.rating + ".";
   } else if (part) {
       var suggestions = partialMatch(term.value);
-      item.textContent = "did you mean " + "'" + part[0].name + "'" + "?"
+      panelBody.textContent = "did you mean " + "'" + part[0].biz + "'" + "?"
   } else {
-    item.textContent = "No review found."
+    panelBody.textContent = "No review found."
   }
-  var position = document.getElementsByClassName('results')[0];
+  var position = document.getElementById('results');
   position.appendChild(item);
 });
+
+
+
+
+//Change below function to use when the user searches for another restaurant:
+// var removeEl = document.getElementsByClassName('container recommended')[0];
+// var containerEl = removeEl.parentNode;
+// containerEl.removeChild(removeEl);
