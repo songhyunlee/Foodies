29af8ae0dcf0acbd6fdd965ceee608b38db36b41
Songhyun Lee <empressivlee@gmail.com>
2016-06-05 18:24:23 -0700

    enhanced search function by adding eventlistener to enter key #1

--- default.js
+++ default.js
@@ -1,9 +1,9 @@
 var reviews = [
   {
-    food: 'american',
+    food: 'American',
     biz: 'Cheezboard',
     description: 'Vegetarian pizzeria serving different pizza everyday...',
-    reviewer: ['Sarah Anderson', 'Two', 'Three'],
+    reviewer: ['Sarah Anderson', 'Reviewer B', 'Reviewer C'],
     review: ['The best vegetarian pizza I have ever had.', 'Lorem ipsum dolor sit amet', 'consectetur adipiscing elit, sed do'],
     rating: 5,
     address: '1 Culver Dr, Irvine, CA 92612',
@@ -12,7 +12,7 @@ var reviews = [
     image: "cheezboard.jpg",
   },
   {
-    food: 'chinese',
+    food: 'Chinese',
     biz: 'Great China',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -24,7 +24,7 @@ var reviews = [
     image: 'greatchina.jpeg',
   },
   {
-    food: 'seafood',
+    food: 'Seafood',
     biz: 'Oh Oyster',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -36,7 +36,7 @@ var reviews = [
     image: 'oysters.jpeg',
   },
   {
-    food: 'italian',
+    food: 'Italian',
     biz: 'Siciliana',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -48,7 +48,7 @@ var reviews = [
     image: 'siciliana.jpg',
   },
   {
-    food: 'korean',
+    food: 'Korean',
     biz: 'Baekjung',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -60,7 +60,7 @@ var reviews = [
     image: 'baekjung.jpg',
   },
   {
-    food: 'japanese',
+    food: 'Japanese',
     biz: 'Sushi Sushi',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -72,7 +72,7 @@ var reviews = [
     image: 'sushi.jpg',
   },
   {
-    food: 'mexican',
+    food: 'Mexican',
     biz: 'Tacos o Burritos',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -84,7 +84,7 @@ var reviews = [
     image: 'burritos.jpg',
   },
   {
-    food: 'foodtruck',
+    food: 'Food truck',
     biz: 'Grab N Go',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -96,7 +96,7 @@ var reviews = [
     image: 'foodtruck.jpg',
   },
   {
-    food: 'dessert',
+    food: 'Dessert',
     biz: 'Matcha Heaven',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -108,7 +108,7 @@ var reviews = [
     image: 'matcha.jpeg',
   },
   {
-    food: 'coffee',
+    food: 'Coffee',
     biz: 'Coffee-to-go',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
     reviewer: ['John Doe', 'Reviewer B', 'Reviewer C'],
@@ -153,8 +153,9 @@ function partialMatch(term) {
   }
 }
 
+//Listen for the click on the 'find' button.
 var myElement = document.getElementById('search')
-myElement.addEventListener('click', function search(Event) {
+myElement.addEventListener('click', function search(e) {
 
   var term = document.getElementById('term');
   var matched = fullMatch(term.value);
@@ -163,16 +164,49 @@ myElement.addEventListener('click', function search(Event) {
   var thePanel = document.createElement('div');
   var panelHeading = document.createElement('div');
   var panelBody = document.createElement('div');
+  var panelSub = document.createElement('div');
   thePanel.setAttribute('class', 'panel panel-success');
   panelHeading.setAttribute('class', 'panel-heading');
   panelBody.setAttribute('class', 'panel-body');
+  panelSub.setAttribute('class', 'panel-body');
   item.setAttribute('class', 'results');
   thePanel.appendChild(panelHeading);
   thePanel.appendChild(panelBody);
   item.appendChild(thePanel);
   if ( matched ) {
     panelHeading.textContent = "Showing results for: " + "'"+term.value +"'. " ;
-    panelBody.textContent = matched.biz + " has a rating of " + matched.rating + ".";
+    var bizName = document.createElement('h4');
+    bizName.textContent = matched.biz;
+    bizName.setAttribute('class', 'biz');
+    var bizCategory = document.createElement('div');
+    bizCategory.textContent = "Category: " + matched.food;
+    bizCategory.setAttribute('class', 'category');
+    var bizAbout = document.createElement('div');
+    bizAbout.textContent = matched.description;
+    bizAbout.setAttribute('class', 'about');
+    var bizWhere = document.createElement('div');
+    bizWhere.textContent = matched.address;
+    bizWhere.setAttribute('class', 'addbox');
+    var bizNumber = document.createElement('div');
+    bizNumber.textContent = matched.phone;
+    bizNumber.setAttribute('class', 'addbox');
+    var reviewOne = document.createElement('div');
+    var reviewTwo = document.createElement('div');
+    var reviewThree = document.createElement('div');
+    reviewOne.textContent = "'" + matched.review[0] + "'" + " - " + matched.reviewer[0];
+    reviewTwo.textContent = "'" + matched.review[1] + "'" + " - " + matched.reviewer[1];
+    reviewThree.textContent = "'" + matched.review[2] + "'" + " - " + matched.reviewer[2];
+
+    panelBody.appendChild(bizName);
+    panelBody.appendChild(bizCategory);
+    panelBody.appendChild(bizWhere);
+    panelBody.appendChild(bizNumber);
+    panelBody.appendChild(bizAbout);
+    panelBody.appendChild(panelSub);
+    panelSub.appendChild(reviewOne);
+    panelSub.appendChild(reviewTwo);
+    panelSub.appendChild(reviewThree);
+
   } else if (part) {
       var suggestions = partialMatch(term.value);
       panelHeading.textContent = "Showing results for: " + "'"+term.value +"'. " ;
@@ -185,6 +219,73 @@ myElement.addEventListener('click', function search(Event) {
   position.appendChild(item);
 });
 
+//Enables search by pressing enter in the input box.
+document.getElementById("term").addEventListener("keydown", function inputSearch (ele) {
+  if (event.keyCode === 13) {
+
+    var term = document.getElementById('term');
+    var matched = fullMatch(term.value);
+    var part = partialMatch(term.value);
+    var item = document.createElement('div');
+    var thePanel = document.createElement('div');
+    var panelHeading = document.createElement('div');
+    var panelBody = document.createElement('div');
+    var panelSub = document.createElement('div');
+    thePanel.setAttribute('class', 'panel panel-success');
+    panelHeading.setAttribute('class', 'panel-heading');
+    panelBody.setAttribute('class', 'panel-body');
+    panelSub.setAttribute('class', 'panel-body');
+    item.setAttribute('class', 'results');
+    thePanel.appendChild(panelHeading);
+    thePanel.appendChild(panelBody);
+    item.appendChild(thePanel);
+    if ( matched ) {
+      panelHeading.textContent = "Showing results for: " + "'"+term.value +"'. " ;
+      var bizName = document.createElement('h4');
+      bizName.textContent = matched.biz;
+      bizName.setAttribute('class', 'biz');
+      var bizCategory = document.createElement('div');
+      bizCategory.textContent = "Category: " + matched.food;
+      bizCategory.setAttribute('class', 'category');
+      var bizAbout = document.createElement('div');
+      bizAbout.setAttribute('class', 'about');
+      bizAbout.textContent = matched.description;
+      var bizWhere = document.createElement('div');
+      bizWhere.textContent = matched.address;
+      bizWhere.setAttribute('class', 'addbox');
+      var bizNumber = document.createElement('div');
+      bizNumber.textContent = matched.phone;
+      bizNumber.setAttribute('class', 'addbox');
+      var reviewOne = document.createElement('div');
+      var reviewTwo = document.createElement('div');
+      var reviewThree = document.createElement('div');
+      reviewOne.textContent = "'" + matched.review[0] + "'" + " - " + matched.reviewer[0];
+      reviewTwo.textContent = "'" + matched.review[1] + "'" + " - " + matched.reviewer[1];
+      reviewThree.textContent = "'" + matched.review[2] + "'" + " - " + matched.reviewer[2];
+
+      panelBody.appendChild(bizName);
+      panelBody.appendChild(bizCategory);
+      panelBody.appendChild(bizWhere);
+      panelBody.appendChild(bizNumber);
+      panelBody.appendChild(bizAbout);
+      panelBody.appendChild(panelSub);
+      panelSub.appendChild(reviewOne);
+      panelSub.appendChild(reviewTwo);
+      panelSub.appendChild(reviewThree);
+
+    } else if (part) {
+        var suggestions = partialMatch(term.value);
+        panelHeading.textContent = "Showing results for: " + "'"+term.value +"'. " ;
+        panelBody.textContent = "did you mean " + "'" + part[0].biz + "'" + "?"
+    } else {
+      panelHeading.textContent = "Showing results for: " + "'"+term.value +"'. " ;
+      panelBody.textContent = "No review found."
+    }
+    var position = document.getElementById('results');
+    position.appendChild(item);
+
+    };
+});
 
 
 
